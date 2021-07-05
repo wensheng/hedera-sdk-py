7b00b84c281676eaacf3b5c0582d37a5f031754e
Wensheng Wang <wenshengwang@gmail.com>
2021-07-05 13:20:31 -0500

    token balances

--- examples/get_account_balance.py
+++ examples/get_account_balance.py
@@ -1,5 +1,8 @@
 from hedera import AccountBalanceQuery
 from get_client import client, OPERATOR_ID
 
-balance = AccountBalanceQuery().setAccountId(OPERATOR_ID).execute(client).hbars
-print("balance = ",  balance.toString())
+balance = AccountBalanceQuery().setAccountId(OPERATOR_ID).execute(client)
+print("Hbar balance = ",  balance.hbars.toString())
+tokens = balance.tokens
+for tokenId in tokens.keySet().toArray():
+    print("Token {} = {}".format(tokenId.toString(), tokens[tokenId]))
